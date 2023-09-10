ed570720bad64c1f1e61b030e87251efcdbc2b20
Katsiaryna <kataarosina@gmail.com>
2023-09-10 11:58:00 +0300

    load image task

--- app/src/main/java/com/example/viewshomework/MainActivity.kt
+++ app/src/main/java/com/example/viewshomework/MainActivity.kt
@@ -2,10 +2,54 @@ package com.example.viewshomework
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import coil.Coil
+import coil.request.ImageRequest
+import com.example.viewshomework.databinding.ActivityMainBinding
+import android.view.inputmethod.EditorInfo
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val editText = binding.editText
+
+        editText.setOnEditorActionListener { _, actionId, _ ->
+            if (actionId == EditorInfo.IME_ACTION_DONE) {
+                val imageUrl = editText.text.toString()
+                loadImage(imageUrl)
+                true
+            } else {
+                false
+            }
+        }
+    }
+
+    private fun loadImage(imageUrl: String) {
+        val request = ImageRequest.Builder(this)
+            .data(imageUrl)
+            .target(
+                onSuccess = { result ->
+                    binding.imageView.setImageDrawable(result)
+                },
+                onError = { error ->
+                    binding.imageView.setImageResource(R.drawable.placeholder)
+                    val errorMessage = "Ошибка при загрузке изображения"
+                    showToast(errorMessage)
+                }
+            )
+            .build()
+
+        Coil.imageLoader(this).enqueue(request)
+    }
+
+    private fun showToast(message: String) {
+        Toast.makeText(this@MainActivity, message, Toast.LENGTH_SHORT).show()
     }
 }
